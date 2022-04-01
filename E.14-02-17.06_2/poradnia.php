e393a62028468dd8cec6a6777da36c17d3fc6347
Azgeda <79477777+Azgeda-debug@users.noreply.github.com>
2022-04-01 14:08:23 +0200

    Add files via upload

---
+++ E.14-02-17.06_2/poradnia.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Poradnia</title>
+    <link rel="stylesheet" type="text/css" href="poradnia.css">
+</head>
+<body>
+    <div id="baner">
+        <h1>PORADNIA SPECJALISTYCZNA</h1>
+    </div>
+    <div id="lewy">
+        <h3>LEKARZE SPECJALIŚCI</h3>
+        <table>
+            <tr>
+                <td colspan="2">Poniedziałek</td>
+            </tr>
+            <tr>
+                <td>Anna Kowalska</td>
+                <td>otolaryngolog</td>
+            </tr>
+            <tr>
+                <td colspan="2">Wtorek</td>
+            </tr>
+            <tr>
+                <td>Jan Nowak</td>
+                <td>kardiolog</td>
+            </tr>
+        </table>
+        <h3>LISTA PACJENTÓW</h3>
+        <?php skrypt1(); ?><br><br>
+        <form action="pacjent.php" method="post">
+            <label for="id">Podaj id:</label><br>
+            <input type="text" name="id" id="id">
+            <input type="submit" value="Pokaż szczegóły">
+        </form>
+    </div>
+    <div id="prawy">
+        <h2>KARTA PACJENTA</h2>
+        <p>Nie wybrano pacjenta</p>
+    </div>
+    <div id="stopka">
+        <p>utworzone przez: 0000000000</p>
+        <a href="kwerendy.txt" download>Kwerendy do pobrania</a>
+    </div>
+</body>
+</html>
+
+<?php
+
+function skrypt1()
+{
+    $conn = mysqli_connect('localhost','root','','poradnia');
+    if(!$conn)
+    {
+        exit('Błąd połączenia z serwerem');
+    }
+    else
+    {
+        $query = mysqli_query($conn, "SELECT id, imie, nazwisko, choroba FROM `pacjenci`;");
+        while($result = mysqli_fetch_array($query))
+        {
+            echo $result[0].' '.$result[1].' '.$result[2].' '.$result[3].'<br>';
+        }
+    }
+    mysqli_close($conn);
+}
+
+?>
